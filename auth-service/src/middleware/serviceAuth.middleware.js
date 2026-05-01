502140f46c414bb56ec95031507aeeb5fddd22b8
PRIYANGSU BANIK <136341993+NxtNinja@users.noreply.github.com>
2026-05-01 11:51:41 +0530

    Update serviceAuth.middleware.js

--- auth-service/src/middleware/serviceAuth.middleware.js
+++ auth-service/src/middleware/serviceAuth.middleware.js
@@ -7,8 +7,6 @@ const AppError = require("../utils/AppError");
 const serviceAuth = (req, res, next) => {
   const serviceKey = req.headers["x-internal-service-key"];
   const expectedKey = process.env.INTERNAL_SERVICE_KEY
-  console.log(expectedKey);
-  
 
   if (!serviceKey || serviceKey !== expectedKey) {
     console.warn(`[SECURITY WARNING] Unauthorized direct service access attempt from IP: ${req.ip}`);
